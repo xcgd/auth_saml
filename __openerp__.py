9e5229865497c3586253bd0c77196b425f44b677
Vincent Hatakeyama <vincent.hatakeyama@xcg-consulting.fr>
2015-06-08 18:47:59 +0200

    3.1

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Saml2 Authentication',
-    'version': '3.0',
+    'version': '3.1',
     'category': 'Tools',
     'description': """
 Allow users to login through Saml2 Provider.
